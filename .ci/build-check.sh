c4799b23d5057053656fb663a3dec3241bffaa21
Андрей Andrean <andreanpal@gmail.com>
2018-06-27 14:02:05 +0500

    add run test

--- .ci/build-check.sh
+++ .ci/build-check.sh
@@ -7,5 +7,9 @@ make
 cp ./bin/gost.so ${PREFIX}/lib/engines-1.1
 
 export LD_LIBRARY_PATH=${PREFIX}/lib
+export PATH=${PREFIX}/bin:${PATH}
+export OPENSSL_ENGINES=${PREFIX}/lib/engines-1.1
 
-${PREFIX}/bin/openssl ciphers |grep GOST
+# ${PREFIX}/bin/openssl ciphers |grep GOST
+
+prove -r test || (cat tests.err && exit 1)
